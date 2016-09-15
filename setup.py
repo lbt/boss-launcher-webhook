40462ccfdbe8f9e5725a21dc8012aa784a3fd040
David Greaves <david@dgreaves.com>
2016-09-15 10:54:37 +0100

    Fix setup.py to install templatetags - explains why some bits are missing!
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@
 # Copyright (C) 2013 Jolla Ltd.
 # Contact: Islam Amer <islam.amer@jollamobile.com>
 # All rights reserved.
-# 
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -23,7 +23,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 static_files=[('/etc/skynet', ['src/webhook_launcher/webhook.conf']),
-                ('/usr/share/webhook_launcher/processes', 
+                ('/usr/share/webhook_launcher/processes',
                    ['src/webhook_launcher/processes/VCSCOMMIT_NOTIFY',
                     'src/webhook_launcher/processes/VCSCOMMIT_BUILD',
                     'src/webhook_launcher/processes/VCSCOMMIT_QUEUE',
@@ -40,14 +40,18 @@ setup(
     author = 'Islam Amer <pharon@gmail.com>',
     packages = ['webhook_launcher',
                 'webhook_launcher.app',
+                'webhook_launcher.app.templatetags',
                 'webhook_launcher.app.migrations',
-                ],    
+                ],
     package_dir = {'webhook_launcher':'src/webhook_launcher',
                    'webhook_launcher.app':'src/webhook_launcher/app',
                    'webhook_launcher.app.migrations' : 'src/webhook_launcher/app/migrations',
                   },
     package_data = { 'webhook_launcher.app' : ['templates/admin/*.html',
                                             'templates/app/*.html',
+                                            'static/*.gif',
+                                            'static/*.css',
+                                            'static/*.js',
                                             ]
                    },
     data_files = static_files,
